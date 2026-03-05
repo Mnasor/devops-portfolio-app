526866b04a9890ece406b6ce07c07d2acd9ad8c9
Mnasor <mnasor2009@outlook.sa>
2026-03-05 14:49:03 +0000

    feat: initial commit with Flask app and Dockerfile

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(
+        status="Success",
+        message="Hello Mansour! Your DevOps Pipeline is working perfectly.",
+        version="1.0"
+    )
+
+if __name__ == '__main__':
+    # التطبيق سيعمل على المنفذ 5000 ويستقبل الاتصالات من أي IP
+    app.run(host='0.0.0.0', port=5000)
