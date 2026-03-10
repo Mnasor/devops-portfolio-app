1179350f71d4c00b0ec86a8f095f39e5d7c94630
Mnasor <mnasor2009@outlook.sa>
2026-03-10 14:12:25 +0000

    fix: separate healthcheck endpoint to clean metrics

--- app.py
+++ app.py
@@ -43,3 +43,10 @@ def home():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+@app.route('/health')
+def health():
+    return "Healthy", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
